3c1b8468b6addec1d0fc2aa5e370f7cda6b021d8
ms-maheswari <mahes7439@gmail.com>
2024-10-15 09:43:12 +0530

    first commit

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = "https://chatbot-with-email-validation-backend.onrender.com"
-export const BACKEND_URL = "http://localhost:5001"
+export const BACKEND_URL = "https://securemailauth.onrender.com"
