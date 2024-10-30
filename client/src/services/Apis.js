30d1a4b43a5960cd0e2422ac42f60aa5e831ef72
MAHESWARI M <124769438+ms-maheswari@users.noreply.github.com>
2024-10-30 22:54:26 +0530

    Update Apis.js

--- client/src/services/Apis.js
+++ client/src/services/Apis.js
@@ -1,7 +1,6 @@
 import { commonrequest } from "./ApiCall";
 import {BACKEND_URL} from "./helper";
 
-
 export const registerfunction = async(data)=>{
     return await commonrequest("POST",`${BACKEND_URL}/user/register`,data)
 }
